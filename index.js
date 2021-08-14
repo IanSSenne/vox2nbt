dfc182e6e315ef78b844c7068150b63ef33f974a
IanSSenne <48780301+IanSSenne@users.noreply.github.com>
2021-08-14 06:38:27 -0700

    support default block

--- index.js
+++ index.js
@@ -60,9 +60,7 @@ for (let i = 0; i < files.length; i++) {
     }
     let p = files[i];
     const data = pmv(fs.readFileSync(p));
-    console.log(data);
     const colors = data.RGBA.map(toHex);
-    debugger;
     const nbt = {
       type: "compound",
       name: "",
@@ -110,6 +108,26 @@ for (let i = 0; i < files.length; i++) {
         },
       },
     };
+    if(config._){
+      console.log("filling default block");
+      const b = nbt.value.blocks.value.value
+      let idx = 0;
+      const total = data.SIZE.x * data.SIZE.y * data.SIZE.z;
+      for(let x = 0;x<data.SIZE.x;x++){
+        for(let y = 0;y<data.SIZE.y;y++){
+          for(let z = 0;z<data.SIZE.z;z++){
+            idx++;
+            if(idx%100===0){
+              process.stdout.write(((idx/total)*100).toFixed(2)+"%")
+              process.stdout.cursorTo(0);
+            }
+            if(b.find(block=>block.pos.value[0] === x && block.pos.value[1] === z && block.pos.value[2] === y)){
+              b.push(createBlock(x,z,y,config._));
+            }
+          }
+        }
+      }
+    }
     fs.writeFileSync(p.replace(/\.vox$/g,"") + ".nbt", zlib.gzipSync(nbtlib.writeUncompressed(nbt)));
   } catch (e) {
     console.log(`error converting file '${files[i]}'`);
